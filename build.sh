bec5a55610b7463061e1ca07f205520a0b2faf3f
VBRiquelme <briquelmec26@gmail.com>
2026-04-20 17:28:44 -0400

    crear superusuario desde env vars

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata datos.json
+python manage.py createsuperuser --noinput --username bryan --email $DJANGO_SUPERUSER_EMAIL || true
