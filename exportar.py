32104513fb02195018d3ec5e28faa007e240a005
Bryan RiquelmeC <briquelmec26@gmail.com>
2026-09-04 11:12:06 -0400

    agregar imagen a fichas final

--- exportar.py
+++ exportar.py
@@ -7,9 +7,9 @@ django.setup()
 
 from django.core.management import call_command
 
-with io.open('nuevas_fichas.json', 'w', encoding='utf-8') as f:
+with io.open('datos.json', 'w', encoding='utf-8') as f:
     call_command('dumpdata', 'accounts', 'fichas',
                  '--natural-foreign', '--natural-primary',
                  '--indent', '2', stdout=f)
 
-print("✅ Exportado correctamente en nuevs_fichas.json")
+print("✅ Exportado correctamente en datos.json")
